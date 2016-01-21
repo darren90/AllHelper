e0f3a8638d828285b09a3fbf18832bcdbf5fde87
darren90 <1005052145@qq.com>
2016-01-21 21:44:32 +0800

    init server data

--- AllHelper/AllHelper/ViewController.swift
+++ AllHelper/AllHelper/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        Alamofire.request(.GET, "https://httpbin.org/get")
+        Alamofire.request(.GET, "http://japi.juhe.cn/joke/content/list.from?key=79cd72cadcdc3a2739cbb84d1364dd6d&page=2&pagesize=10&sort=asc&time=1418745237", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
